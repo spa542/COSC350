5e0eb08c4f9fed06fa6b06302daf8b6fb406cd3b
Ryan Rosiak <ryan11291129@gmail.com>
2021-04-22 13:58:50 -0400

    Finished task 1, got two more tasks to go

--- Lab10/Task1/msgQrcv.c
+++ Lab10/Task1/msgQrcv.c
@@ -8,6 +8,15 @@
 #include<stdlib.h> // Standard Library
 #include<sys/msg.h> // Message Queue Stuff
 #include<sys/types.h> // Types
+#include<sys/ipc.h> // IPC Stuff
+#include<stdbool.h> // bool alias
+
+#define BUFFER_SIZE 2
+
+typedef struct my_msgbuff {
+    long mtype;
+    int buff[2];
+} msgbuff;
 
 /*
  * Receive a message (two integers) from the message queue created by msgQsnd.c
@@ -16,5 +25,42 @@
  */
 int main(void) {
 
+    // Key value
+    key_t key;
+    // Message queue id
+    int msqid;
+    // Receive buffer
+    int rcvBuff[BUFFER_SIZE];
+    // Check byte
+    int nbyte;
+    // Check
+    int check;
+    // Get the msgid_ds structure
+    struct msqid_ds data;
+    // Receive
+    msgbuff rcv;
+
+    // Tokenize the message queue
+    if ((key = ftok("msgQsnd.c", 'B')) == -1) {
+        puts("*** Error tokenizing the message queue ***");
+        return 1; // Returning because there was an error tokenizing the message queue
+    }
+
+    // Get the message queue
+    if ((msqid = msgget(key, 0644)) == -1) {
+        puts("*** Error getting the message queue ***");
+        return 2; // Returning because there was an error getting the message queue
+    }
+
+    // Get the messages from the message queue
+    while (true) {
+        if ((check = msgrcv(msqid, (msgbuff*)&rcv, sizeof(rcv), 0, 0)) == -1) {
+            puts("*** Error receiving the message ***");
+            return 4; // Returning because there was an error receiving the message
+        }
+        printf("%d + %d = %d\n", rcv.buff[0], rcv.buff[1], rcv.buff[0]+rcv.buff[1]);
+    }
+
+    // All done!
     return 0;
 }
--- Lab10/Task1/msgQsnd.c
+++ Lab10/Task1/msgQsnd.c
@@ -8,6 +8,16 @@
 #include<stdlib.h> // Standard Library
 #include<sys/msg.h> // Message Queue Stuff
 #include<sys/types.h> // Types
+#include<unistd.h> // read()
+#include<string.h> // sscanf()
+#include<sys/ipc.h> // IPC Stuff
+
+#define BUFFER_SIZE 80
+
+typedef struct my_msgbuff {
+    long mtype;
+    int buff[2];
+} msgbuff;
 
 /*
  * Create message queue with rw-r-r. To create a message queue, use existing 
@@ -18,5 +28,53 @@
  */
 int main(void) {
 
+    // Message queue id
+    int msqid;
+    // Integer variables to send to the queue
+    int sendBuff[2];
+    // Key value
+    key_t key;
+    // Read buffer
+    char buffer[BUFFER_SIZE];
+    // Byte check
+    int nbyte;
+    // Check
+    int check;
+    // Message buffer
+    msgbuff send;
+
+    // Get the key for the message queue
+    if ((key = ftok("msgQsnd.c", 'B')) == -1) {
+        puts("*** Error tokenizing the file ***");
+        return 1; // Returning because there was an error tokenizing the file
+    }
+
+    // Create the message queue
+    if ((msqid = msgget(key, 0644 | IPC_CREAT)) == -1) {
+        puts("*** Error creating the message queue ***");
+        return 2; // Returning because there was an error creating the message queue
+    }
+
+    // Read from the user until CTR-D
+    while ((nbyte = read(0, buffer, BUFFER_SIZE)) > 0) {
+        if (sscanf(buffer, "%d%d", &sendBuff[0], &sendBuff[1]) == 2) {
+            send.mtype = 1;
+            send.buff[0] = sendBuff[0];
+            send.buff[1] = sendBuff[1];
+            if ((check = msgsnd(msqid, (msgbuff*)&send, sizeof(send), 0)) == -1) {
+                puts("*** Error sending message ***");
+            }
+        } else {
+            puts("*** Invalid inputs (must be two integers) ***");
+        }
+    }
+
+    // Remove the message queue
+    if ((check = msgctl(msqid, IPC_RMID, NULL)) == -1) {
+        puts("*** Error removing the message queue ***");
+        return 3; // Returning because there was an error removing the message queue
+    }
+
+    // All done!
     return 0;
 }
